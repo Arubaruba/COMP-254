5c1d481e847f1cc9eac51a53ffad5e20f8c4a685
Andreas Stocker <shadowstep7@gmail.com>
2015-12-18 18:43:49 +0200

    Complete Example 5

--- Ex05.asm
+++ Ex05.asm
@@ -6,7 +6,7 @@ Title (Ex05.asm)
 Abytes1 db 10h, 20h, 30h, 40h
 Abytes1_len = $ - Abytes1
 Awords1 dw 100h, 200h, 300h, 400h
-Awords1_len = $ - Awords1
+Awords1_len = ($ - Awords1) / 2
 
 Res1a db ?
 Res1b db ?
@@ -16,14 +16,79 @@ Res3 dw ?
 
 .code
 main proc
+
+	; Part A
+	mov ax, 0
+
+	mov edi, offset Abytes1
 	
-	mov ebx, Awords1_len
+	; Add contents of Abytes1 WITHOUT a loop
+	add al, [edi]
+	inc edi
+	add al, [edi]
+	inc edi
+	add al, [edi]
+	inc edi
+	add al, [edi]
+
+	mov Res1a, al
+
+	; Part B
+	mov ax, 0
+
+	mov edi, offset Abytes1
 	mov ecx, Abytes1_len
+
+	; Add contents of Abytes1 WITH a loop
+
+	loop1:
+	add al, [edi]
+	inc edi
+	Loop loop1
+
+	mov Res1b, al
+
+	; Part C
+	mov ax, 0
+
+	mov edi, offset Awords1
 	
-	mov eax, 0
-	mov al, Abytes1[2]
-	
-	add eax, 10
+	; Add contents of Awords1 WITHOUT a loop
+	add ax, [edi]
+	add edi, 2
+	add ax, [edi]
+	add edi, 2
+	add ax, [edi]
+	add edi, 2
+	add ax, [edi]
+
+	mov Res2a, ax
+
+	; Part D
+	mov ax, 0
+
+	mov edi, offset Awords1
+	mov ecx, Awords1_len
+
+	; Add contents of Awords1 WITH a loop
+
+	loop2:
+	add ax, [edi]
+	add edi, 2
+	Loop loop2
+
+	mov Res2b, ax
+
+	; Part E
+	mov ax, 0
+
+	; Add Res2b and Res1a
+	add al, Res1a
+	add ax, Res2b
+
+	mov Res3, ax
+
+	mov ax, 0
 
 main endp
 end main
